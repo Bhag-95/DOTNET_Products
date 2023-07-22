5cf1e3d8865d8ee0a0f1366ae059b10ec8d735e7
Bhagyesh Lunawat <106422436+Bhag-95@users.noreply.github.com>
2023-07-22 19:18:17 +0530

    Add files via upload

---
+++ BOL/Product.cs
@@ -0,0 +1,67 @@
+﻿namespace BOL
+{
+    public enum Category
+    {
+        BISCUITS,Chips,COLDDRINKS
+    }
+    public class Product
+    {
+        private int id;
+        private string name;
+        private int quantity;
+        private Category category;
+        private double price;
+
+
+        public Product()
+        {
+
+        }
+
+        public Product(int id,string name, int quantity, Category category, double price)
+        {
+            this.id = id;
+            this.name = name;
+            this.quantity=quantity;
+            this.category = category;
+            this.price = price;
+        }
+
+        public int ID
+        {
+            get { return this.id; }
+            set { this.id = value; }
+        }
+
+        public String Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+
+        }
+
+        public int Quantity
+        {
+            get { return this.quantity; }
+            set { this.quantity = value; }
+        }
+
+        public Category CATEGORY
+        {
+            get { return this.category; }
+            set { this.CATEGORY = value; }
+
+        }
+        public double Price
+        {
+            get { return this.price; }
+            set { this.price = value; }
+        }
+
+        public override string ToString()
+        {
+            return "Id is: "+id+ "Name is: " + name + "Quantity: " + quantity + " Category: " + category + " Price: " + price;
+        }
+
+    }
+}
---
+++ DAL/DBManager.cs
@@ -0,0 +1,99 @@
+﻿using BOL;
+using MySql.Data.MySqlClient;
+namespace DAL
+{
+    public class DBManager
+    {
+        public static string connString = @"server=localhost;port=3306;user=root;password=root123;database=bhagyesh";
+
+        public static List<Product> getAllProducts()
+        {
+            List<Product> products = new List<Product>();
+            MySqlConnection conn = new MySqlConnection(connString);
+
+            try
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = conn;
+                string query = "select * from products";
+                cmd.CommandText = query;
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    int id = int.Parse(reader["id"].ToString());
+                    string name = reader["panme"].ToString();
+                    int quantity = int.Parse(reader["qty"].ToString());
+                    Category category = Enum.Parse<Category>(reader["category"].ToString());
+                    double price = double.Parse(reader["price"].ToString());
+
+                    Product product = new Product(id, name, quantity, category, price);
+                    products.Add(product);
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally { conn.Close(); }
+
+            return products;
+        }
+
+        public static void insertProduct(Product product)
+        {
+            MySqlConnection conn = new MySqlConnection(connString);
+
+            try
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = conn;
+                string query = "insert into products values('" + product.ID + "','" + product.Name + "','" + product.Quantity + "','" + product.CATEGORY + "','" + product.Price + "')";
+                cmd.CommandText = query;
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            finally { conn.Close(); }
+
+
+        }
+
+        public static String deleteProduct(int id)
+        {
+            MySqlConnection conn = new MySqlConnection(connString);
+            String mesg = "Not deleted";
+            try
+            {
+
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = conn;
+                string query = "Delete from products where id=" + id;
+                cmd.CommandText = query;
+                cmd.ExecuteNonQuery();
+                mesg = "deleted";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+            finally 
+            { 
+                conn.Close(); 
+            }
+            return mesg;
+        }
+    }
+        
+}
