e9b6d48afd852a32850cdd4b7b18ecb7c7f080a6
dac <dac@dypm.org>
2023-07-22 19:17:46 +0530

    Products

---
+++ HRWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using HRWebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using BOL;
+using DAL;
+
+namespace HRWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Products()
+        {
+            List<Product> products=DBManager.getAllProducts();
+
+            ViewBag.Products = products;
+
+            return View();
+        }
+
+            public IActionResult Add() { 
+            return View();
+            }
+
+        [HttpPost]
+        public IActionResult Add(string id, string name,string qty, string category,string price)
+        {
+            Product newProd = new Product(int.Parse(id), name, int.Parse(qty), Enum.Parse<Category>(category), double.Parse(price));
+            Console.WriteLine(newProd);
+            DBManager.insertProduct(newProd);
+            
+            return RedirectToAction("Products");
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+           // Console.WriteLine(id);
+            DBManager.deleteProduct(id);
+            return RedirectToAction("Products");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
